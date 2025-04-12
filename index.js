2883d80603446eba8040b00a83eec4ecb91e8cce
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2025-04-11 21:52:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { FormMoeda, FormData, FormText } from "srodrigo28_project"
+
+console.log(FormMoeda(100.99))
+console.log(FormData('2020-10-10'))
+console.log(FormText('queria muito ser um dos melhores programdores', 20))
